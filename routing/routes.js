dac843f6f5645387fa2437bfa6f97c1b6aa06e7d
Joel Godoy <joelgiovanni4@gmail.com>
2019-07-10 22:15:24 -0600

    Post routes have been added on backend

--- routing/routes.js
+++ routing/routes.js
@@ -5,6 +5,9 @@ const passport = require('passport');
 var jwt = require('jsonwebtoken');
 const keys = require('../config/Keys');
 const saltRounds = 12;
+const mongoose = require('mongoose');
+
+var ObjectId = mongoose.Types.ObjectId;
 
 const registerDate = require('../helpers/DateStamp');
 
@@ -14,24 +17,6 @@ const Post = require('../models/Post');
 //Validation for the login / register routes
 const registerValidation = require('../validation/register');
 const validateLogin = require('../validation/login');
-
-router.post('/newpost', (req, res) => {
-  const newPost = new Post({
-    title: req.body.title,
-    author: req.body.author,
-    body: req.body.body,
-    date: registerDate
-  });
-  newPost
-    .save()
-    .then(
-      res.json({
-        success: true,
-        message: 'You have added a new post!'
-      })
-    )
-    .catch(err => console.log(err));
-});
 
 // @route   POST
 // @desc    Register a new user
@@ -144,5 +129,118 @@ router.get(
     });
   }
 );
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////// Code below is for adding | viewing | deleting posts /////////////////////////////////////////
+
+// @route   POST
+// @desc    Create a new post
+// @access  Private
+// URL: http://localhost:5000/auth/newpost
+router.post('/newPost', (req, res) => {
+  const name = req.body.author;
+
+  //Check to see if the Author aka User exists
+  User.findOne({ name })
+    .then(user => {
+      if (user) {
+        const newPost = new Post({
+          title: req.body.title,
+          author: req.body.author,
+          body: req.body.body,
+          date: registerDate
+        });
+        newPost.save().then(
+          res.json({
+            success: true,
+            message: 'You have added a new post!'
+          })
+        );
+      } else {
+        // If the user does not exist , send error message
+        return res.status(400).json({
+          email: 'Oops, That user does not exist!'
+        });
+      }
+    })
+    .catch(err => console.log(err));
+});
+
+// @route   GET
+// @desc    Use user's name to fetch all posts by that user
+// @access  Private
+// URL: http://localhost:5000/auth/loadUserPosts
+router.get('/loadUserPosts', (req, res) => {
+  const author = req.body.name;
+
+  Post.find({ author: author })
+    .then(posts => {
+      // If the users posts are less than or equal to zero. the 'error' message will appear
+      if (posts.length <= 0) {
+        res.json({
+          message: 'There is no posts for this user. Or the user does not exist'
+        });
+      } else {
+        res.json(posts);
+      }
+    })
+    .catch(err => console.log(err));
+});
+
+// @route   DELETE
+// @desc    Delete a individual post
+// @access  Private
+// URL: http://localhost:5000/auth/loadUserPosts
+router.delete('/deletePost', (req, res) => {
+  const { id } = req.body;
+
+  Post.findByIdAndDelete(ObjectId(req.body.id))
+    .then(post => {
+      if (post) {
+        res.json({
+          success: true,
+          postDeleted: `${post.title}`,
+          postAuthor: `${post.author}`
+        });
+      } else {
+        res.json({
+          success: false,
+          message: 'Unable to find that post'
+        });
+      }
+    })
+    .catch(err => console.log(err));
+});
+
+// @route   POST
+// @desc    Update a individual post
+// @access  Private
+// URL: http://localhost:5000/auth/updatePost
+router.post('/updatePost', (req, res) => {
+  const { id } = req.body;
+
+  Post.findByIdAndUpdate(
+    id,
+    { $set: { body: req.body.newBody } },
+    { new: true, useFindAndModify: false }
+  )
+    .then(updatedPost => {
+      if (updatedPost) {
+        res.json(updatedPost);
+      } else {
+        res.json({ success: false });
+      }
+    })
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
