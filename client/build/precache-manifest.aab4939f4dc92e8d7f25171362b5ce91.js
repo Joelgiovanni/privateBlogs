34d17663c996b1988fb7468efa5e51ad32301899
Joel Godoy <joelgiovanni4@gmail.com>
2019-07-21 23:41:16 -0600

    .gitignore changes

---
+++ client/build/precache-manifest.aab4939f4dc92e8d7f25171362b5ce91.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7ce394efac21d868a66f8f6bd7210d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc4f958028869c51895b",
+    "url": "/static/css/main.4cdd9158.chunk.css"
+  },
+  {
+    "revision": "b4d2f2c7599de32ba154",
+    "url": "/static/js/2.b1205806.chunk.js"
+  },
+  {
+    "revision": "bc4f958028869c51895b",
+    "url": "/static/js/main.edf61bb9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6ae18e9a65c4d9fc66d6ab9c038c9f60",
+    "url": "/static/media/img01.6ae18e9a.jpeg"
+  },
+  {
+    "revision": "44d5b239e0badf51f17ba35341bc32b8",
+    "url": "/static/media/img02.44d5b239.jpeg"
+  },
+  {
+    "revision": "c0a5f5f66613f236eba65ddfed4cd806",
+    "url": "/static/media/img03.c0a5f5f6.jpeg"
+  },
+  {
+    "revision": "2ea5cd489202d2a359eb081aa1a5ab0f",
+    "url": "/static/media/img04.2ea5cd48.jpeg"
+  },
+  {
+    "revision": "153950f1f8152fa1e7f0e4e687ae2c03",
+    "url": "/static/media/img05.153950f1.jpeg"
+  },
+  {
+    "revision": "4f98d856ae0b08e76d44635348ac86db",
+    "url": "/static/media/img06.4f98d856.jpeg"
+  }
+]);
---
+++ config/Keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  URI: 'mongodb://joelg4:privateblog1@ds243607.mlab.com:43607/private-blog',
+  secretTokenKey: 'QuickBrownFoxAndQickDevCollab'
+};
